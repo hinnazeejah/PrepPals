d6320a9be7b445261b64e9684d89e63b5a212f5f
hinnazeejah <129240862+hinnazeejah@users.noreply.github.com>
2024-03-18 06:11:59 -0400

    prompt.py

--- prompt.py
+++ prompt.py
@@ -1,11 +1,7 @@
-# from sentence_transformers import SentenceTransformer
 from openai import OpenAI
 import os
 import numpy as np
 from dotenv import load_dotenv
-
-# Load a Hugging Face model
-# model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')  # Specify the model name you want to use
 
 load_dotenv()
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -22,10 +18,9 @@ def get_cosine_similarity(embedding1, embedding2):
     return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
 
 
-# Function to split the text into paragraphs
 def split_into_paragraphs(text):
     paragraphs = text.split('\n\n')
-    # Filter out any empty paragraphs or ones that are too short to be meaningful
+    
     return [para for para in paragraphs if len(para) > 40]
 
 
@@ -35,10 +30,10 @@ def get_most_relevant_embeddings(embeddings, query_embedding):
         sim = get_cosine_similarity(query_embedding, embeddings[e])
         similarities.append((sim, e))
 
-    # Sort by similarity in descending order
+
     similarities.sort(reverse=True, key=lambda x: x[0])
 
-    # Get top 10 similarities
+
     return similarities[:10]
 
 
@@ -52,7 +47,7 @@ def query_model(website_data, resume_data):
     {website_data}
     """
 
-    prompt = f"""You are an expert at preparing for interviews at Big Tech companies. I am a Computer Science student that is preparing for interviews. Given the information about the company that I am interviewing with below, generate an agenda for a 30 minute interview with the company. 
+    prompt = f"""You are an expert at preparing for interviews at Big Tech companies. I am a Cybersecurity student that is preparing for interviews. Given the information about the company that I am interviewing with below, generate an agenda for a 30 minute interview with the company. 
 
     For each section of the agenda, include specific topics to discuss based on my resume and the company's website. 
              
